f11c4a341f6e2c6fa2bcf0bba281a8438f98f0f8
jsergent <julien.a.sergent@capgemini.com>
2017-02-28 15:58:24 +0100

    fix: Remove useless test file

--- t.js
+++
@@ -1,9 +0,0 @@
-async function t() {
-    return new Promise( resolve => setTimeout( () => resolve( 'Hello World!' ), 2000 ) )
-}
-
-async function p() {
-    console.log( await t() ) // Hello World!
-}
-
-p()
