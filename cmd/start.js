d5a55b34170cb6a0977ce5a4f196811304dafac3
jsergent <julien.a.sergent@capgemini.com>
2017-03-06 14:48:17 +0100

    feat(start): Enable interactive start command
    Handle correct output colors, and handle user interaction (ctrl+c,
    keywords: stop, start, ex, exit, rs, restart)

--- cmd/start.js
+++ cmd/start.js
@@ -7,8 +7,84 @@
 * file that was distributed with this source code.
 */
 
+const spawn = require( 'child_process' ).spawn
+const chokidar = require( 'chokidar' )
+const readline = require( 'readline' )
+
 module.exports.command = 'start'
 module.exports.describe = 'Start application'
 module.exports.handler = argv => {
-    require(`${easy.easyPath}/server`)
+    let applicationProcess = spawn( 'node', [ 'node_modules/easy/server.js' ], { stdio: 'inherit' })
+    const watcher = chokidar.watch( `${easy.appRootPath}/src`, { persistent: true })
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    watcher.on( 'change', path => {
+        applicationProcess = restartApplication( applicationProcess )
+    })
+
+    rl.setPrompt( '' )
+    rl.prompt()
+    rl.on( 'line', line => {
+        const restartCommands = [ 'rs', 'restart' ]
+        const stopCommands = [ 'stop' ]
+        const startCommand = [ 'start' ]
+        const exitCommand = [ 'ex', 'exit' ]
+
+        line = line.trim().toLowerCase()
+
+        if ( restartCommands.includes( line ) ) {
+            console.log( '\nRestarting the application...\n' )
+            applicationProcess = restartApplication( applicationProcess )
+        } else if ( stopCommands.includes( line ) ) {
+            console.log( '\nStopping the application...' )
+            stopApplication( applicationProcess )
+            console.log( 'Application is stopped\n' )
+        } else if ( startCommand.includes( line ) ) {
+            console.log( '\nStarting the application...\n' )
+            applicationProcess = startApplication()
+        } else if ( exitCommand.includes( line ) ) {
+            console.log( '\nStopping the application...' )
+            process.emit( 'SIGINT' )
+        }
+    })
+    rl.on( 'SIGINT', () => process.emit( 'SIGINT' ) )
+
+    process.on( 'SIGINT', () => {
+        stopApplication( applicationProcess )
+        console.log( 'Exiting easy cli...' )
+        process.exit()
+    })
+}
+
+/**
+ * Restart the application
+ *
+ * @param {ChildProcess} applicationProcess
+ * @returns {ChildProcess}
+ */
+function restartApplication( applicationProcess ) {
+    stopApplication( applicationProcess )
+
+    return startApplication()
+}
+
+/**
+ * Stop the application
+ *
+ * @param {ChildProcess} applicationProcess
+ */
+function stopApplication( applicationProcess ) {
+    applicationProcess.kill()
+}
+
+/**
+ * Start the application
+ *
+ * @returns {ChildProcess}
+ */
+function startApplication() {
+    return spawn( 'node', [ 'node_modules/easy/server.js' ], { stdio: 'inherit' })
 }
